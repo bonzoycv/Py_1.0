a986891c93a91a4ce0bf4f2aee7877be24b0f81d
Yahir <bonzoycv@yandex.com>
2022-08-05 13:32:53 +0200

    Add files via upload

---
+++ forloop_.py
@@ -0,0 +1,16 @@
+
+#!/usr/bin/env python3
+
+
+#for loob basics with random numbers
+
+
+import random
+
+counter = random.randint(5, 10)
+
+number = 1
+
+for i in range(counter):
+	print (number)
+	number += 1
